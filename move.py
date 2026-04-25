c1d6f766798a002c0e54780a19d25250fa51bedc
aokenu <34852856+aokenu@users.noreply.github.com>
2026-04-25 10:00:00 +0100

    Updated project file

--- move.py
+++ move.py
@@ -9,4 +9,4 @@ path1 = "C:/python_work/Billingsample/Billingsample/"
 
 #pad the full source path where the original file exist
 src_folder=f"{path1}{date}"
-dst_folder = 
+dst_folder = f'C:/python_work/sample/3line Card management Limited_{date}_155959/ '
