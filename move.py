8a593d663544426403dd4339c8bb230fb1c89468
aokenu <34852856+aokenu@users.noreply.github.com>
2026-07-08 10:00:00 +0100

    Updated project file

--- move.py
+++ move.py
@@ -46,3 +46,4 @@ path1 = "C:/python_work/Billingsample/Billingsample/"
 #path2="_040219"
 src_folder=f"{path1}{date}"
 dst_folder = f'C:/python_work/sample/AB Microfinance bank_{date}_155959/ '
+print(src_folder)
