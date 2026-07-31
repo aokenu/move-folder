ca7ed19d002445e449b1ee6586c2256160645767
aokenu <34852856+aokenu@users.noreply.github.com>
2026-07-31 21:48:56 +0100

    updated project file

--- move.py
+++ move.py
@@ -52,5 +52,6 @@ print(file_names)
 
 ##print(src_folder)
 #file_names = os.listdir(src_folder)
+#print('move:', file)
 
 #loop through the list to move files with with a unique pattern (src_folder + "/3*") 
